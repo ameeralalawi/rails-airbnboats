c55ba2b71146416647f6314a3bf685d6982e0287
Gaetan May <gaetan.may@gmail.com>
2017-08-15 16:31:38 +0100

    add some changes on the show of the boat

--- app/controllers/boats_controller.rb
+++ app/controllers/boats_controller.rb
@@ -1,9 +1,12 @@
 class BoatsController < ApplicationController
   def show
+    @boat = Boat.find(params[:id])
   end
 
   def results
   end
+
+  private
 
   def boat_params
     params.require(:boat).permit(photos: [])
