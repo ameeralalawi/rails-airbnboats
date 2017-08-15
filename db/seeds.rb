242a35a970f4865006fcd50a877378af4cc00c35
Charles Cazals <charlescazals@gmail.com>
2017-08-15 10:58:51 +0100

    seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
+
+  user = User.create ({
+    :firstname => first_name,
+    :lastname => last_name,
+    :email => "#{first_name}.#{last_name}@test.com",
+    :password => "password"
+    })
+end
