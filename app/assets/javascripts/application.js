280847f8e3b93f8774e2815a41391e232b3c642f
ameer alalawi <asalalawi@gmail.com>
2017-08-16 09:19:51 +0100

    First version of the landing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,8 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require bootstrap-sprockets
-//= require_tree .
-
-
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
-
+//= require_tree .
