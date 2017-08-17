83ce9e8734ffa52dede99e905e85fc4e1c34c135
Gaetan May <gaetan.may@gmail.com>
2017-08-18 00:37:44 +0100

    the date matching is working except the view redirection in case of conflict

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,26 +6,38 @@ class BookingsController < ApplicationController
     @booking.default_values
     @booking.save
     redirect_to boats_booking_confirm_path(@booking.boat_id, @booking.id)
-    raise
   end
 
   def index
   end
 
   def confirm
-    raise
-    # @boat = Boat.find(params[:id])
-    # dates = []
-    # @boat.booking_slots.each do |booking_slot|
-    #   dates << booking_slot.availability_slot.date.strftime("%d-%m-%Y")
-    # end
-    # @blocked_dates = dates
+    #First, we want to put the booking dates into a range (15ofjuly..18ofjuly).
+    @booking= Booking.new
+    @booking.start_date = params[:chkin]
+    @booking.end_date = params[:chkout]
+    desired = @booking.start_date..@booking.end_date
+
+    #We want to have an array of all the booking date ranges already existing for this boat.
+    @booking.boat_id = params[:id]
+    @boat = Boat.find(params[:id])
+    booked = []
+    @boat.bookings.each do |booking|
+      booked << (booking.start_date..booking.end_date)
+    end
+
+    #Third, we want to compare our new booking date range with all the one existing and accept or refuse booking accordingly.
+    booked.each do|booker|
+      if booker.overlaps?(desired)
+          redirect_to(:action => "show") and return
+      end
+    end
   end
 
-private
+  private
 
-def booking_params
-params.require(:booking).permit(:start_date, :end_date)
+  def booking_params
+    params.require(:booking).permit(:chkin, :chkout)
   end
 
 end
